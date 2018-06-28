19f46c3bb4d4e223494fca5a1ac4054b41c58a24
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-06-28 12:04:44 -0700

    made style changes based on Google Material Design guidelines: i.e. button colors, shape, font

--- react-client/src/components/Decrement.jsx
+++ react-client/src/components/Decrement.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Decrement = (props) => {
   return (
     <div id="decrement">
-      <a className="waves-effect waves-light btn-large pink lighten-4 buttons" 
+      <a className="waves-effect waves-light btn-large purple darken-2 buttons" 
          onClick={()=> {props.onClick('decrement')}}>
          Decrease
       </a>
--- react-client/src/components/Increment.jsx
+++ react-client/src/components/Increment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Increment = (props) => {
   return (
     <div id="increment">
-      <a className="waves-effect waves-light btn-large pink lighten-4 buttons"
+      <a className="waves-effect waves-light btn-large purple darken-2 buttons"
          onClick={()=> {props.onClick('increment')}}>
          Increase
       </a>
